2edb6f302d6f32c53e852769dbe2438c67b692c7
Patrick Stephan <pstephan1187@gmail.com>
2024-06-06 15:55:59 -0500

    Cache wp-json

--- public/sw.ts
+++ public/sw.ts
@@ -1,5 +1,9 @@
 import {cleanupOutdatedCaches, precacheAndRoute} from 'workbox-precaching';
 import {imageCache, offlineFallback, pageCache, staticResourceCache} from "workbox-recipes";
+import {registerRoute} from "workbox-routing";
+import {StaleWhileRevalidate} from "workbox-strategies";
+import {CacheableResponsePlugin} from "workbox-cacheable-response";
+import {ExpirationPlugin} from "workbox-expiration";
 
 cleanupOutdatedCaches();
 
@@ -8,6 +12,16 @@ precacheAndRoute(self.__WB_MANIFEST);
 pageCache({warmCache: ['/', '/about', '/sponsors', '/map', '/artists']});
 imageCache({maxEntries: 500});
 staticResourceCache();
+
+registerRoute(
+    ({request}) => request.url.includes('wp-json'),
+    new StaleWhileRevalidate({
+        plugins: [
+            new CacheableResponsePlugin({statuses: [0, 200]}),
+            new ExpirationPlugin({maxAgeSeconds: 60 * 60 * 24 * 30}),
+        ],
+    })
+);
 
 offlineFallback({
     pageFallback: '/',
