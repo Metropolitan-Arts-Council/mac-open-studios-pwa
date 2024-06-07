ff0346d9327c50ecd953b3b4c2361825bc4e7567
Patrick Stephan <pstephan1187@gmail.com>
2024-06-07 06:44:53 -0500

    wp-json api requests aren't being cached properly. trying this

--- public/sw.ts
+++ public/sw.ts
@@ -1,7 +1,7 @@
 import {cleanupOutdatedCaches, precacheAndRoute} from 'workbox-precaching';
 import {imageCache, offlineFallback, pageCache, staticResourceCache} from "workbox-recipes";
 import {registerRoute} from "workbox-routing";
-import {StaleWhileRevalidate} from "workbox-strategies";
+import {NetworkFirst} from "workbox-strategies";
 import {CacheableResponsePlugin} from "workbox-cacheable-response";
 
 cleanupOutdatedCaches();
@@ -10,7 +10,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 
 registerRoute(
     ({request}) => request.url.includes('wp-json'),
-    new StaleWhileRevalidate({
+    new NetworkFirst({
         plugins: [
             new CacheableResponsePlugin({statuses: [0, 200]}),
         ],
