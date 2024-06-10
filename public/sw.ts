e653ffb72f559f542adbbdeb46e860dbf6baec31
Patrick Stephan <pstephan1187@gmail.com>
2024-06-10 12:28:25 -0500

    Some fix attempts

--- public/sw.ts
+++ public/sw.ts
@@ -4,31 +4,81 @@ import {registerRoute} from "workbox-routing";
 import {NetworkFirst} from "workbox-strategies";
 import {CacheableResponsePlugin} from "workbox-cacheable-response";
 
+class CustomLoggerPlugin {
+    response = undefined;
+
+    async cacheWillUpdate(ctx) {
+        // console.log('cacheWillUpdate', ctx);
+        return ctx.response;
+    }
+    async cacheKeyWillBeUsed(ctx) {
+        // console.log('cacheKeyWillBeUsed', ctx);
+        return ctx.request.clone();
+    }
+    async cachedResponseWillBeUsed(ctx) {
+        const response = this.response ? this.response : ctx.cachedResponse;
+        // console.log('cachedResponseWillBeUsed', ctx, response);
+        return response;
+    }
+    async requestWillFetch(ctx) {
+        // console.log('requestWillFetch', ctx);
+        return ctx.request;
+    }
+    async fetchDidFail(ctx) {
+        // console.log('fetchDidFail', ctx);
+    }
+    async fetchDidSucceed(ctx) {
+        // console.log('fetchDidSucceed', ctx, ctx.response.ok);
+
+        if (ctx.response.ok) {
+            this.response = await ctx.response.clone();
+            return this.response;
+        }
+    }
+    async handlerWillRespond(ctx) {
+        // console.log('handlerWillRespond', ctx);
+        return ctx.response;
+    }
+    async handlerDidRespond(ctx) {
+        // console.log('handlerDidRespond', ctx);
+    }
+    async handlerDidComplete(ctx) {
+        // console.log('handlerDidComplete', ctx);
+    }
+    async handlerDidError(ctx) {
+        // console.log('handlerDidError', ctx);
+        return null;
+    }
+}
+
 const manifest = self.__WB_MANIFEST;
 cleanupOutdatedCaches();
 
 offlineFallback({
-    pageFallback: '/',
+    pageFallback: 'index.html',
     imageFallback: 'placeholder.png',
 });
 
 precacheAndRoute(manifest);
 
+// self.addEventListener('install', () => console.log('SW Install', manifest));
+
 registerRoute(
-    ({request}) => request.url.includes('wp-json') || request.url.includes('_payload.json'),
+    ({url}) => url.pathname.includes('wp-json') || url.pathname.includes('_payload.json'),
     new NetworkFirst({
         plugins: [
             new CacheableResponsePlugin({statuses: [0, 200]}),
+            new CustomLoggerPlugin(),
         ],
     })
 );
 
 pageCache({warmCache: [
-    '/', '/index.html', 'index.html',
-    '/about', 'about', '/about.html', 'about.html',
-    '/sponsors', 'sponsors', '/sponsors.html', 'sponsors.html',
-    '/map', 'map', '/map.html', 'map.html',
-    '/artists', 'artists', '/artists.html', 'artists.html',
-]});
+    '/',
+    '/about',
+    '/sponsors',
+    '/map',
+    '/artists',
+], plugins: [new CustomLoggerPlugin()]});
 imageCache({maxEntries: 500});
-staticResourceCache();
+staticResourceCache(/*{plugins: [new CustomLoggerPlugin()]}*/);
