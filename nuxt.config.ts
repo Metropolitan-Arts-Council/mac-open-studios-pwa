a5951478c97c942018b2deb06e7bb5c92c132ff5
Patrick Stephan <pstephan1187@gmail.com>
2024-06-12 13:52:27 -0500

    Turned off SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  ssr: false,
   modules: ['@pinia/nuxt', "@vite-pwa/nuxt"],
   alias: {
     pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
