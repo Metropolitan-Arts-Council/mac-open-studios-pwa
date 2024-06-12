06227994f1cb29d4297712bf25dd661a378811a2
Patrick Stephan <pstephan1187@gmail.com>
2024-06-12 14:29:10 -0500

    removed comments

--- public/sw.ts
+++ public/sw.ts
@@ -4,53 +4,6 @@ import {registerRoute} from "workbox-routing";
 import {NetworkFirst} from "workbox-strategies";
 import {CacheableResponsePlugin} from "workbox-cacheable-response";
 
-// class CustomLoggerPlugin {
-//     response = undefined;
-//
-//     async cacheWillUpdate(ctx) {
-//         console.log('cacheWillUpdate', ctx);
-//         return ctx.response;
-//     }
-//     async cacheKeyWillBeUsed(ctx) {
-//         console.log('cacheKeyWillBeUsed', ctx);
-//         return ctx.request.clone();
-//     }
-//     async cachedResponseWillBeUsed(ctx) {
-//         const response = this.response ? this.response : ctx.cachedResponse;
-//         console.log('cachedResponseWillBeUsed', ctx, response);
-//         return response;
-//     }
-//     async requestWillFetch(ctx) {
-//         console.log('requestWillFetch', ctx);
-//         return ctx.request;
-//     }
-//     async fetchDidFail(ctx) {
-//         console.log('fetchDidFail', ctx);
-//     }
-//     async fetchDidSucceed(ctx) {
-//         console.log('fetchDidSucceed', ctx, ctx.response.ok);
-//
-//         if (ctx.response.ok) {
-//             this.response = await ctx.response.clone();
-//             return this.response;
-//         }
-//     }
-//     async handlerWillRespond(ctx) {
-//         console.log('handlerWillRespond', ctx);
-//         return ctx.response;
-//     }
-//     async handlerDidRespond(ctx) {
-//         console.log('handlerDidRespond', ctx);
-//     }
-//     async handlerDidComplete(ctx) {
-//         console.log('handlerDidComplete', ctx);
-//     }
-//     async handlerDidError(ctx) {
-//         console.log('handlerDidError', ctx);
-//         return null;
-//     }
-// }
-
 const manifest = self.__WB_MANIFEST;
 cleanupOutdatedCaches();
 
@@ -63,21 +16,18 @@ pageCache({
         '/sponsors',
         '/map',
         '/artists',
-    ]/*, plugins: [new CustomLoggerPlugin()]*/
+    ]
 });
-
-// self.addEventListener('install', () => console.log('SW Install', manifest));
 
 registerRoute(
     ({url}) => url.pathname.includes('wp-json') || url.pathname.includes('_payload.json'),
     new NetworkFirst({
         plugins: [
             new CacheableResponsePlugin({statuses: [0, 200]}),
-            // new CustomLoggerPlugin(),
         ],
     })
 );
-staticResourceCache(/*{plugins: [new CustomLoggerPlugin()]}*/);
+staticResourceCache();
 imageCache({maxEntries: 500});
 offlineFallback({
     pageFallback: 'index.html',
