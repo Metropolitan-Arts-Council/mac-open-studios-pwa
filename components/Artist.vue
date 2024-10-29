587cb63f5129968da2afc655e7e32ef7b75087c4
Patrick Stephan <pstephan1187@gmail.com>
2024-10-29 10:22:29 -0500

    Added the "Not Participating" field to the artist component

--- components/Artist.vue
+++ components/Artist.vue
@@ -1,90 +1,96 @@
 <template>
-    <div class="results-item">
-        <div class="results-item-preview" v-if="showPreview">
-            <img
-                class="results-item-image"
-                :src="imgSrc"
-                alt="Photo of artist"
-                @click="goToProfile"
-            />
-            <button
-                class="results-item-favorite button-blank"
-                :class="{ active: isFavorited }"
-                @click="favorite"
-            >
-                <img src="~/assets/icons/favorite-outline.png" v-show="!isFavorited" />
-                <img src="~/assets/icons/favorite-filled.png" v-show="isFavorited" />
-                <span>Favorite</span>
-            </button>
-        </div>
-        <div class="results-item-info">
-            <div class="results-item-title-container">
-              <h3 class="results-item-title" @click="goToProfile">
-                {{ artist.full_name }}
-              </h3>
-              <a v-if="showPreview && artist.youtube_link" class="results-item-title-youtube" :href="artist.youtube_link" target="_blank">
-                <img src="~/assets/icons/youtube.png" alt="YouTube" />
-              </a>
-            </div>
-            <span class="results-item-num" v-show="artist.listing_number">{{ artist.listing_number }}</span>
-            <div class="results-item-tags">
-            <span class="results-item-accessible-tag" v-if="artist.handicap_accessible">
-                <img src="~/assets/icons/accessible-circle.png" alt="Accessible" />
-            </span>
-            <!-- <span class="results-item-friday-tag" v-if="artist.open_friday">
-                <img src="~/assets/icons/friday-circle.png" alt="Open Friday" />
-                Open Friday
-            </span> -->
-            <span class="results-item-friday-tag results-item-appointment-tag" v-if="artist.preview && artist.preview !== 'No'">
-                <img src="~/assets/icons/preview-filter.png" alt="By Appointment" />
-                <strong>Friday Preview</strong>
-            </span>
-            <span class="results-item-friday-tag results-item-appointment-tag" v-if="artist.appointment && artist.appointment !== 'No'">
-                <img src="~/assets/icons/appointment-only.png" alt="By Appointment" />
-                <strong>By Appointment:&nbsp;</strong> {{ artist.appointment }}
-            </span>
-            </div>
-            <address class="results-item-address" v-if="showAddress">
-                {{ street }}
-                <br />
-                {{ address.city + ',' + address.state_zip }}
-                <br>
-                <span @click="openMap">Get Directions</span>
-            </address>
-            <p v-if="showAddress">{{ artist.phone_number }}</p>
-            <div class="results-item-links" v-if="showAddress">
-                <a :href="mailto" v-if="artist.email_address" target="_blank">{{ artist.email_address }}</a>
-                <br />
-                <a :href="website" v-if="artist.website" target="_blank">{{ hostname }}</a>
-            </div>
-            <a v-if="!showPreview && artist.youtube_link" class="results-item-youtube" :href="artist.youtube_link" target="_blank">
-                <img src="~/assets/icons/youtube.png" alt="YouTube" />
-                <strong>YouTube</strong>
-            </a>
-            <footer>
-                Medium:
-                <span v-for="(medium, index) in artist.mediums" class="artist-medium">
-                    <span v-html="medium"></span><span v-if="index != (artist.mediums.length - 1)">, </span>
-                </span>
-            </footer>
-            <footer v-if="useDates && artist.days && artist.days.length">
-                <div v-for="{day, hours} in artist.days">
-                  {{ formatDate(day) }}: <span class="artist-medium">{{ hours }}</span>
-                </div>
-            </footer>
-            <div v-if="showFavorite" class="results-item-favorite-block">
-                <button
-                    class="results-item-favorite button-blank"
-                    :class="{ active: isFavorited }"
-                    @click="favorite"
-                >
-                    <img src="~/assets/icons/favorite-outline.png" v-show="!isFavorited" />
-                    <img src="~/assets/icons/favorite-filled.png" v-show="isFavorited" />
-                    <span>Favorite</span>
-                </button>
-            </div>
-        </div>
+  <div class="results-item">
+    <div class="results-item-preview" v-if="showPreview">
+      <img
+        class="results-item-image"
+        :src="imgSrc"
+        alt="Photo of artist"
+        @click="goToProfile"
+      />
+      <button
+        class="results-item-favorite button-blank"
+        :class="{ active: isFavorited }"
+        @click="favorite"
+      >
+        <img src="~/assets/icons/favorite-outline.png" v-show="!isFavorited"/>
+        <img src="~/assets/icons/favorite-filled.png" v-show="isFavorited"/> <span>Favorite</span>
+      </button>
     </div>
+    <div class="results-item-info">
+      <div class="results-item-title-container">
+        <h3 class="results-item-title" @click="goToProfile">
+          {{ artist.full_name }}
+        </h3>
+        <a
+          v-if="showPreview && artist.youtube_link"
+          class="results-item-title-youtube"
+          :href="artist.youtube_link"
+          target="_blank"
+        > <img src="~/assets/icons/youtube.png" alt="YouTube"/> </a>
+      </div>
+      <span class="results-item-num" v-show="artist.listing_number">{{ artist.listing_number }}</span>
+      <div class="results-item-tags">
+        <span class="results-item-accessible-tag" v-if="artist.handicap_accessible">
+          <img src="~/assets/icons/accessible-circle.png" alt="Accessible"/>
+        </span>
+        <!-- <span class="results-item-friday-tag" v-if="artist.open_friday">
+            <img src="~/assets/icons/friday-circle.png" alt="Open Friday" />
+            Open Friday
+        </span> -->
+        <span
+          class="results-item-friday-tag results-item-appointment-tag"
+          v-if="artist.preview && artist.preview !== 'No'"
+        >
+          <img src="~/assets/icons/preview-filter.png" alt="By Appointment"/>
+          <strong>Friday Preview</strong>
+          </span> <span
+        class="results-item-friday-tag results-item-appointment-tag"
+        v-if="artist.appointment && artist.appointment !== 'No'"
+      >
+          <img src="~/assets/icons/appointment-only.png" alt="By Appointment"/>
+          <strong>By Appointment:&nbsp;</strong> {{ artist.appointment }}
+        </span>
+      </div>
+      <div class="results-item-not-participating" v-if="artist.not_participating">
+        <img src="~/assets/icons/calendar-close.png" alt="Unable to Participate"/> Unable to Participate
+      </div>
+      <address class="results-item-address" v-if="showAddress">
+        {{ street }} <br/> {{ address.city + ',' + address.state_zip }} <br>
+        <span @click="openMap">Get Directions</span>
+      </address>
+      <p v-if="showAddress">{{ artist.phone_number }}</p>
+      <div class="results-item-links" v-if="showAddress">
+        <a :href="mailto" v-if="artist.email_address" target="_blank">{{ artist.email_address }}</a> <br/>
+        <a :href="website" v-if="artist.website" target="_blank">{{ hostname }}</a>
+      </div>
+      <a
+        v-if="!showPreview && artist.youtube_link"
+        class="results-item-youtube"
+        :href="artist.youtube_link"
+        target="_blank"
+      > <img src="~/assets/icons/youtube.png" alt="YouTube"/> <strong>YouTube</strong> </a>
+      <footer>
+        Medium: <span v-for="(medium, index) in artist.mediums" class="artist-medium">
+            <span v-html="medium"></span><span v-if="index != (artist.mediums.length - 1)">, </span>
+          </span>
+      </footer>
+      <footer v-if="useDates && artist.days && artist.days.length">
+        <div v-for="{day, hours} in artist.days">
+          {{ formatDate(day) }}: <span class="artist-medium">{{ hours }}</span>
+        </div>
+      </footer>
+      <div v-if="showFavorite" class="results-item-favorite-block">
+        <button
+          class="results-item-favorite button-blank"
+          :class="{ active: isFavorited }"
+          @click="favorite"
+        >
+          <img src="~/assets/icons/favorite-outline.png" v-show="!isFavorited"/>
+          <img src="~/assets/icons/favorite-filled.png" v-show="isFavorited"/> <span>Favorite</span>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -150,11 +156,11 @@ const website = computed(() => {
     : 'http://' + props.artist.website
 });
 const hostname = computed(() => {
-    let tmp = document.createElement('a');
-    tmp.href = website.value;
+  let tmp = document.createElement('a');
+  tmp.href = website.value;
 
-    return tmp.hostname;
-  });
+  return tmp.hostname;
+});
 const imgSrc = computed(() => {
   return props.showImage ? props.artist.image : props.artist.image_thumb;
 });
@@ -173,10 +179,9 @@ const openMap = () => {
     location = `maps.google.com/maps?daddr=${lat + ',' + lng}&amp;ll=`;
   }
 
-  if( window.navigator.userAgent.indexOf('Win') !== -1) {
+  if (window.navigator.userAgent.indexOf('Win') !== -1) {
     window.open(`https://${location}`);
-  }
-  else {
+  } else {
     window.open(`maps://${location}`);
   }
 };
@@ -189,7 +194,7 @@ const goToProfile = () => {
 
   router.push({
     name: 'map-artist-show-id',
-    params: { id: props.artist.id }
+    params: {id: props.artist.id}
   })
 };
 const formatDate = (date) => {
